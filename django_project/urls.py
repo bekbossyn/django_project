2d40016fef9ed4d2b9fcebee97c89b8fc63ec88c
bekbossyn <bekbossyn.kassymkhan@gmail.com>
2018-03-04 14:56:36 +0600

    test

--- django_project/urls.py
+++ django_project/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from django_project import views
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^test/$', views.test, name='test'),
 ]
