7a6c041016931c9d756894c6a4f13bdae4de0151
bekbossyn <bekbossyn.kassymkhan@gmail.com>
2018-03-06 23:55:35 +0600

    asdf

--- django_project/views.py
+++ django_project/views.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 
 
 def test(request):
-    return HttpResponse("<h1>Spotis 61</h1> <h1>Daniyar 005</h1>")
+    return HttpResponse("<h1>Spotis 61</h1> <h1>Daniyar 005</h1><h1>two fellas are looking at me</h1>")
 
