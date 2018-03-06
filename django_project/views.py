97731d354b76bfddb27405cf26bfc61cddc2cf9b
bekbossyn <bekbossyn.kassymkhan@gmail.com>
2018-03-06 23:52:16 +0600

    test

--- django_project/views.py
+++ django_project/views.py
@@ -2,4 +2,5 @@ from django.http import HttpResponse
 
 
 def test(request):
-    return HttpResponse("<h1>test for PRODUCTION PsJ Real</h1>")
+    return HttpResponse("<h1>Spotis 61</h1> <h1>Daniyar 005</h1>")
+
